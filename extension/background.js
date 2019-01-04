28cdd481b010cf9c6ecbaf9ae4b1e39d41cf3b67
MarcNY <mnh.2403@gmail.com>
2019-01-05 00:20:45 +0100

    cleaned background.js

--- extension/background.js
+++ extension/background.js
@@ -1,168 +1,17 @@
-class xmsClient {
-  constructor(apiUrl) {
-    this.xmsAPIAuthToken = null;
-    this.xmsApiUrl = apiUrl;
-
-    try {
-      console.log("Getting Auth Token from XMS API");
-
-      this.getLatestAuthorizationKey()
-        .then(token => {
-          this.xmsAPIAuthToken = token;
-          //console.log('XMS Auth Token = ' + this.xmsAPIAuthToken)
-        })
-        .catch(function(err) {
-          console.log("Failed to get token");
-          console.table(err);
-        });
-    } catch (error) {
-      console.log("Error getting auth token " + error);
-    }
-  }
-
-  getLatestAuthorizationKey() {
-    const oauthApplicationId = "aus2eaqpr0kZ9gg2f0i7";
-    const oktaOAuthEndpoint = "";
-    var authHeaders = {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Cache-Control": "no-cache"
-    };
-    var data = {
-      grant_type: "client_credentials",
-      client_id: "0oa2eauwer5y529M40i7",
-      client_secret: "XbqpNLN6DcUkiD1opm_4a7FMJKECV-JlyleT31yM"
-    };
-    var authToken = null;
-
-    var query = "";
-    for (var key in data) {
-      query +=
-        encodeURIComponent(key) + "=" + encodeURIComponent(data[key]) + "&";
-    }
-
-    return new Promise((resolve, reject) => {
-      fetch(oktaOAuthEndpoint, {
-        method: "POST",
-        headers: authHeaders,
-        body: query
-      })
-        .then(res => res.json())
-        .then(response => {
-          authToken = response.access_token;
-          resolve(authToken);
-        })
-        .catch(function(error) {
-          console.log(error);
-          reject(error);
-        });
-    });
-  }
-
-  sendOrder(quantity, price, tag) {
-    const xmsOrderEndpoint = this.xmsApiUrl + "/order";
-    var authHeaders = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "Cache-Control": "no-cache",
-      "X-Api-Authorization": "Bearer " + this.xmsAPIAuthToken
-    };
-
-    var orderData = {
-      side: "BUY",
-      security_id_source: "ID",
-      security_id: "6070",
-      quantity: quantity,
-      order_type: "LIMIT",
-      limit_price: price,
-      timeinforce: "GOOD_TILL_CANCEL",
-      props: {},
-      account_id: 0,
-      tag: tag,
-      source: "nico_api",
-      market_connector: "bitmex"
-    };
-
-    return new Promise((resolve, reject) => {
-      fetch(xmsOrderEndpoint, {
-        method: "POST",
-        headers: authHeaders,
-        body: JSON.stringify(orderData)
-      })
-        .then(res => {
-          if (res.status >= 200 && res.status < 400) {
-            resolve(res);
-          } else {
-            reject(res);
-          }
-        })
-        .catch(function(error) {
-          console.log(error);
-          reject(error);
-        });
-    });
-  }
-
-  getInstrument(symbol, exchange) {
-    var authHeaders = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "Cache-Control": "no-cache",
-      "X-Api-Authorization": "Bearer " + this.xmsAPIAuthToken
-    };
-
-    var query = "";
-    query += "symbol=" + encodeURIComponent(symbol) + "&";
-    query += "exchange_id=" + encodeURIComponent(exchange);
-
-    console.log("Token: ", this.xmsAPIAuthToken);
-
-    var xmsInstrumentRequestUrl = this.xmsApiUrl + "/instrument?" + query;
-
-    return new Promise((resolve, reject) => {
-      fetch(xmsInstrumentRequestUrl, {
-        method: "GET",
-        headers: authHeaders
-      })
-        .then(res => {
-          if (res.status >= 200 && res.status < 400) {
-            resolve(res);
-          } else {
-            reject(res);
-          }
-        })
-        .catch(function(error) {
-          console.log(error);
-          reject(error);
-        });
-    });
-  }
-}
-
 // Platform
-const URL = "www.tradingview.com";
 
 // Timing
 const TIME_POLL = 1000;
 const TIME_STATUS = 5000;
 const T0 = parseInt((new Date("2018.12.09").getTime() / 1000).toFixed(0));
 const ORANGE_POLL_MULTIPLIER = 5;
-
+const URL = "tradingview.com/chart";
 // Trading
 const DEFAULT_K = 5000.0;
 const DEFAULT_TF = 60;
 const DEFAULT_X = 20;
-const EXCHANGES = [
-  "binance",
-  "bitfinex",
-  "bitmex",
-  "bitstamp",
-  "deribit",
-  "gdax",
-  "okex"
-];
 
 // API
-var api = new xmsClient("https://xms.dev.elwood.systems/api");
 
 var lastrcv = 0;
 var tab_ids = [];
@@ -293,6 +142,7 @@ function getTimeNow() {
 }
 
 function tabManager() {
+  console.log("Tab Manager Called");
   tab_ids = [];
   tab_n_polls = [];
   tab_n_recvs = [];
@@ -307,10 +157,12 @@ function poll() {
 }
 
 function status() {
+  //console.log("status_listen", status_listen);
+  //console.log("tab ids length", tab_ids.length);
   if (status_listen == false) return;
   ok = 0;
   for (var j = 0; j < tab_ids.length && ok == 0; j++) {
-    //console.log(j + ' '+ tab_n_polls[j] +' '+ tab_n_recvs[j]);
+    console.log(j + " " + tab_n_polls[j] + " " + tab_n_recvs[j]);
     if (tab_n_polls[j] == tab_n_recvs[j]) ok = 1;
   }
   if (ok == 1) {
@@ -355,13 +207,15 @@ function reset() {
 }
 
 function getRelevantTabs(winData) {
+  console.log("Calling getRelevantTabs");
   for (var i in winData) {
     var winTabs = winData[i].tabs;
     var totTabs = winTabs.length;
     for (var j = 0; j < totTabs; j++) {
+      //console.log(winTabs[j].url);
       if (
-        typeof winTabs[j].favIconUrl !== "undefined" &&
-        winTabs[j].favIconUrl.includes(URL)
+        typeof winTabs[j].url !== "undefined" &&
+        winTabs[j].url.includes(URL)
       ) {
         tab_ids.push(winTabs[j].id);
         tab_n_polls.push(0);
@@ -465,140 +319,7 @@ function group() {
   return g;
 }
 
-function customAlerts() {
-  return;
-  //L1 conditions
-  db_add([
-    "ABC",
-    "n=entryL1 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    1,
-    "06/12/2018 17:17:22"
-  ]);
-  db_add([
-    "ABC",
-    "n=entryL1 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    1,
-    "06/12/2018 17:17:22"
-  ]);
-  db_add([
-    "ABC",
-    "n=entryL1 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    1,
-    "06/12/2018 17:17:22"
-  ]);
-  db_add([
-    "ABC",
-    "n=addL1 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:23"
-  ]);
-  db_add([
-    "ABC",
-    "n=exitL1 e=bitmex d=short t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:24"
-  ]);
-  db_add([
-    "ABC",
-    "n=stopL1 e=bitmex d=short t=l s=XBTUSD m=false p=-0.01 q=0",
-    0,
-    "06/12/2018 17:17:25"
-  ]);
-  //S1 conditions
-  db_add([
-    "ABC",
-    "n=entryS1 e=bitmex d=short t=l s=XBTUSD m=true p=0 q=4",
-    0,
-    "06/12/2018 17:17:26"
-  ]);
-  db_add([
-    "ABC",
-    "n=addS1 e=bitmex d=short t=l s=XBTUSD m=false p=0 q=0",
-    0,
-    "06/12/2018 17:17:27"
-  ]);
-  db_add([
-    "ABC",
-    "n=exitS1 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0 tf=50",
-    0,
-    "06/12/2018 17:17:28"
-  ]);
-  db_add([
-    "ABC",
-    "n=stopS1 e=bitmex d=long t=l s=XBTUSD m=false p=0.01 q=0",
-    0,
-    "06/12/2018 17:17:29"
-  ]);
-  //L2 conditions
-  db_add([
-    "ABC",
-    "n=entryL2 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:30"
-  ]);
-  db_add([
-    "ABC",
-    "n=addSL2 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:31"
-  ]);
-  db_add([
-    "ABC",
-    "n=exitL2 e=bitmex d=short t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:32"
-  ]);
-  db_add([
-    "ABC",
-    "n=stopL2 e=bitmex d=short t=l s=XBTUSD m=false p=-0.01 q=0",
-    0,
-    "06/12/2018 17:17:33"
-  ]);
-  //S2 conditions
-  db_add([
-    "ABC",
-    "n=entryS2 e=bitmex d=short t=l x=24 s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:34"
-  ]);
-  db_add([
-    "ABC",
-    "n=addS2 e=bitmex d=short t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:35"
-  ]);
-  db_add([
-    "ABC",
-    "n=exitS2 e=bitmex d=long t=l s=XBTUSD m=true p=0 q=0",
-    0,
-    "06/12/2018 17:17:46"
-  ]);
-  db_add([
-    "ABC",
-    "n=stopS2 e=bitmex d=long t=l s=XBTUSD m=false p=0.01 q=0",
-    0,
-    "06/12/2018 17:17:37"
-  ]);
-
-  db_add([
-    "ABC",
-    "n=stopS2 e=bitmex d=long t=dl s=XBTUSD m=true p=0.01 q=0",
-    0,
-    "06/12/2018 17:17:37"
-  ]);
-  db_add([
-    "ABC",
-    "e=bitmex d=long t=l s=XBTUSD m=true p=0.01 q=0",
-    0,
-    "06/12/2018 17:17:37"
-  ]);
-  db_add([
-    "ABC",
-    "n=addS2 e=bitmex d=short t=l s=XBTUSD m=true p=-1 q=2",
-    0,
-    "06/12/2018 17:17:35"
-  ]);
-}
+function customAlerts() {}
 
 function prepare(msg) {
   title = -1;
@@ -710,6 +431,7 @@ function prepare(msg) {
 }
 
 function updateAlert(id, status) {
+  console.log("updating alerts");
   for (var j = 0; j < alerts_db.length; j++) {
     if (alerts_db[j][5] == id) {
       alerts_db[j][4] = status;
@@ -718,112 +440,4 @@ function updateAlert(id, status) {
   }
 }
 
-function orderAdd(order, id) {
-  title = order[0];
-  direction = order[1];
-  p = order[2];
-  q = order[3];
-  type = order[4];
-  exchange = order[5];
-  symbol = order[6];
-  m = order[7];
-  x = order[8];
-  tf = order[9]; // in minutes
-
-  // Values needed from the API
-  best_price = -1;
-  N = -1;
-  tick_size = -1;
-  price = -1;
-
-  console.log("ADD ORDER: ", [id, p, q]);
-  orders_db.push([
-    id,
-    0,
-    title,
-    direction,
-    p,
-    q,
-    type,
-    exchange,
-    symbol,
-    m,
-    x,
-    tf,
-    [-1, -1, -1]
-  ]);
-
-  // Step 1: get best bid if long or best offer if short
-  api_getBest(id, direction, symbol, exchange)
-    .then(data => {
-      console.log("Got best: ", [id, data]);
-      best_price = data;
-    })
-    .catch(function(err) {
-      console.log(
-        "Failed to get bid/offer for " + [id, direction, symbol, exchange]
-      );
-      console.log(err);
-    });
-
-  if (p != 0) {
-    // Step 2: get tick size
-    // ...
-    price = best_price - p * tick_size;
-  } else {
-    price = best_price;
-  }
-
-  // step 4
-  if (q == 0) {
-    // q2 = get position and out of everything
-  } else {
-    api_getN(id, symbol, exchange)
-      .then(data => {
-        console.log("Got N: ", [id, data]);
-        N = data;
-      })
-      .catch(function(err) {
-        console.log("Failed to get N for " + [id, symbol, exchange]);
-        console.log(err);
-      });
-    q2 = DEFAULT_K / N;
-  }
-
-  //updateAlert(id, 0);
-}
-
-function api_getBest(id, direction, symbol, exchange) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve(234.3);
-    }, 3000);
-  });
-}
-
-function api_getN(id, symbol, exchange) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve(555.3);
-    }, 1000);
-  });
-}
-
 function popup_callback() {}
-
-function sendTestOrder() {
-  const quantity = 1;
-  const price = 2;
-  const tag = "test";
-
-  api
-    .sendOrder(quantity, price, tag)
-    .then(function(result) {
-      console.log("ORDER api result = " + result.status);
-      console.log(result);
-    })
-    .catch(function(err) {
-      console.log("Failed to send order");
-      console.log(err);
-    });
-}
