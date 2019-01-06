114899c84545320b24f6f620ed5891b8deec3bc3
MarcNY <mnh.2403@gmail.com>
2019-01-06 23:40:21 +0100

    function to call execution

--- src/main.py
+++ src/main.py
@@ -1,21 +1,39 @@
 import pika  # client module for rabbit_mq
 import trading.main as tradelib
 import json
+import time
 # function called when receiving a message from amqp
 
 
 def callback(ch, method, properties, body):
-    print(decode_message(body)['value'])
+
+    order_message = decode_message(body)['value']
+    underlying = order_message['underlying']
+    msg = order_message['msg']
+    start_time = time.time()
+    order_id = tradelib.execute_message(underlying, msg)
+    end_time = time.time()
+
+    order_message['order_id'] = order_id
+    order_message['start_execution_time'] = start_time
+    order_message['end_execution_time'] = end_time
+
+    ch.basic_publish(exchange='',
+                     routing_key='executed_signals',
+                     body=json.dumps(order_message))
     # Main function listener
 
 
 def decode_message(body):
     message_received = json.loads(body)
-    if isinstance(message_received, list) and message_received.length == 8:
+    message_received = message_received.replace(
+        '[', '').replace('"', '').replace(']', '').split(',')
+
+    if isinstance(message_received, list) and len(message_received) == 8:
         return {'valid': 1,
                 'value': {'underlying': message_received[0],
                           'msg': message_received[1],
-                          'time': message_received[2]}}
+                          'time': message_received[3]}}
     else:
         return {'valid': 0, 'value': message_received}
 
--- src/test.py
+++ src/test.py
@@ -2,6 +2,11 @@ import json
 
 a = b'"[\\"XBTUSD\\",\\"n=entryL1 d=long t=m p=0 q=1 u=1 c=10000 b=1h\\",10,\\"06/01/2019 21:49:38\\",-2,-1,\\"entryL1\\",\\"n=entryL1 d=long t=m p=0 q=1 u=1 c=10000 b=1h\\"]"'
 b = json.loads(a)
+
+c = b.replace('[', '').replace('"', '').replace(']', '').split(',')
 print(type(b))
 print(isinstance(b, list))
-print(b.length)
+
+
+print(type(c))
+print(c[0])
